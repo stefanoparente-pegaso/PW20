06b723b412cbf10568c8f34c7c720b7a83ccc115
Stefano Parente <parente.stefano16@gmail.com>
2026-03-19 23:38:25 +0100

    Bug fixing

--- src/dataset_utils.py
+++ src/dataset_utils.py
@@ -5,7 +5,10 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+# TODO: come faccio a fare in modo che i log delle installazioni non vengano mostrati ogni volta???
 nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('stopwords')
 
 def embed_dataset(rev_tk_df, vectorizer):
     rev_strings = rev_tk_df.apply(lambda x: " ".join(x))
@@ -23,7 +26,7 @@ def clean_text(text):
     return text
 
 def preprocess_dataset(dataset_path, rows_to_return_percentage, ascending):
-
+    nltk.download('punkt')
     df = pd.read_csv(dataset_path, sep=';')
     df['recensione_completa'] = df['Titolo'] + " " + df['Corpo']
     df['recensione_completa'] = df['recensione_completa'].apply(clean_text)
